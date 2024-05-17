0d097571b03cae5119bd8db11690de628b3fecfd
ZAZiOs <danyabogd6@gmail.com>
2024-05-17 12:40:36 +0300

    minor fixes

--- src/routes/rus_menu/+page.svelte
+++ src/routes/rus_menu/+page.svelte
@@ -29,9 +29,9 @@ let page = 'Wii'
 </div>
 
 <ol>
-<li>Загрузите и отправьте в папку apps на SD-карте <a href="https://hbb1.oscwii.org/api/contents/WiiModLite/WiiModLite.zip">WiiModLite</a></li>
-<li>В архиве с переводом найдите папку с ВАШЕЙ ВЕРСИЕЙ ПРОШИВКИ Wii <small class="text-secondary">(Можно найти в разделе Wii Options → Wii Settings, там будет что-то типо Ver. <b>4.3E</b>)</small></li>
-<li>Загрузите нужный WAD и отправьте на SD-карту в папку WAD</li>
+<li>Загрузите и положите в папку apps на SD-карте <a href="https://hbb1.oscwii.org/api/contents/WiiModLite/WiiModLite.zip">WiiModLite</a></li>
+<li><a href="https://yadi.sk/d/R6tsQsECG66GJQ">В архиве с переводом</a> найдите папку с ВАШЕЙ ВЕРСИЕЙ ПРОШИВКИ Wii <small class="text-secondary">(Можно найти в разделе Wii Options → Wii Settings, там будет что-то типо Ver. <b>4.3E</b>)</small></li>
+<li>Загрузите нужный WAD и положите на SD-карту в папку WAD</li>
 <li>На консоли запустите Homebrew Channel и WiiModLite, в нём выберите WAD Manager → SD Card</li>
 <li>После завершения установки выходите из WiiModLite кнопкой HOME</li>
 <li>Перезагружаете консоль и вуаля! Русский перевод установлен.</li>
@@ -43,7 +43,7 @@ let page = 'Wii'
     <li>В NUS Downloader переходим в Database → System → System Menu и выбираем ВАШУ версию меню Wii.</li>
     <li>Снизу ставим галочку напротив Pack WAD и нажимаем Start NUS Download.</li>
     <li>После завершения скачивания переходим в папку titles → 0000000100000002 → <i>папка&nbsp;с&nbsp;вашей&nbsp;версией</i><br> <small class="text-secondary">(4.1U - 449; 4.1E - 450; 4.2U - 481; 4.2E - 482; 4.3U - 513; 4.3E - 514)</small></li>
-    <li>Загруженный WAD отправьте на SD-карту в папку WAD</li>
+    <li>Загруженный WAD положите на SD-карту в папку WAD</li>
     <li>Далее следуем инструкции по установке меню ↑, начиная с пункта 4.</li>
 </ol>
 
@@ -67,7 +67,7 @@ let page = 'Wii'
     <li>На Wii U в Homebrew Launcher запускаем Mocha, после чего запускаем FTPiiU Everywhere.</li>
     <li>Устанавливаем и запускаем <a href="https://filezilla.ru/get/">FileZilla*</a> на компьютере.<br><small class="text-secondary"><b>*</b> — или любой другой предпочтительный FTP-клиент.</small> </li>
     <li>На Wii U будут отображены IP-адрес и порт. Их нужно вписать сверху в FileZilla: в окошки «хост» и «порт» соответственно. Затем нажимаем «Быстрое соединение».<br><small class="text-secondary">(Нужно чтобы компьютер и Wii U были подключены к одной сети.)</small></li>
-    <li>Снизу появится файловая система Wii U. Нужно перейти в slccmpt01 → title → 00000001 → 00000002 → content. Там будет файл 00000022.app. На всякий случай копируем его куда-нибудь на компьютер, а файл из приложенного архива с таким же названием заменяем на консоли через FileZilla. <br><small class="text-secondary">(Передача файла может занять некоторое время.)</small></li>
+    <li>Снизу появится файловая система Wii U. Нужно перейти в slccmpt01 → title → 00000001 → 00000002 → content. Там будет файл 00000022.app. На всякий случай копируем его куда-нибудь на компьютер, а файл <a href="https://yadi.sk/d/itZ1udIotUt-6A">из приложенного архива</a> <small><a href="https://yadi.sk/d/zm4pqQhJyzuk6w" class="text-secondary">(зеркало)</a></small> с таким же названием заменяем на консоли через FileZilla. <br><small class="text-secondary">(Передача файла может занять некоторое время.)</small></li>
     <li>Переходим в slccmpt01 → shared1. Там будут файлы 0000000d.app и 0000000f.app. Их тоже копируем на компьютер и заменяем файлами из архива.</li>
     <li>После того как все файлы перекинулись на консоль, нажимаем HOME на GamePad и заходим в меню Wii. Там должен появиться русский язык!</li>
 </ol>
